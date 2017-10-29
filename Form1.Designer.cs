8635ceb3863fe493a267fff709c6bd1996d71762
Mike A. Trethewey <miketrethewey@gmail.com>
2017-10-29 03:58:44 -0700

    Bump version number
    Add easy to update Version number from Project File

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -393,7 +393,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "PaletteCreator ";
+            this.Text = "PaletteCreator " + PaletteCreator.Properties.Resources.Version;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
